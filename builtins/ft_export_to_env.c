9dc4318eb08a9db890685c558897cbe52e1ffd9e
groubaud <guillaume.roubaud1@gmail.com>
2022-05-31 16:01:07 +0200

    WIP export
    2 options, on verra dans l'environnement MAC
    faut faire la liste var_bash
    et l'export à partir de var_bash

--- builtins/ft_export_to_env.c
+++ builtins/ft_export_to_env.c
@@ -12,6 +12,30 @@
 
 #include "minishell.h"
 
+static t_env	*ft_find_the_insert_spot(t_env *env)
+{
+	t_env	*tmp_env;
+
+	tmp_env = ft_getenv("WSL_DISTRO_NAME", env); // ce sera pas ça
+	if (tmp_env)
+		return (tmp_env);
+	tmp_env = ft_getenv("LANG", env);
+	if (tmp_env)
+		return (tmp_env);
+	tmp_env = ft_getenv("LESSCLOSE", env);
+	if (tmp_env)
+		return (tmp_env);
+	tmp_env = ft_getenv("HOSTTYPE", env);
+	if (tmp_env)
+		return (tmp_env);
+	tmp_env = ft_getenv("LS_COLORS", env);
+	if (tmp_env)
+		return (tmp_env);
+	while (env->next)
+		env = env->next;
+	return (env);
+}
+
 static t_env	*ft_add_new_elem_env(t_env *env, char *name, char *value)
 {
 	t_env	*env_new;
@@ -22,10 +46,18 @@ static t_env	*ft_add_new_elem_env(t_env *env, char *name, char *value)
 	env_new->len_name = ft_strlen(name);
 	env_new->value = value;
 	env_new->next = NULL;
+	if (env == NULL)
+		return (env_new);
 	start = env;
-	if (env)
-		while (env->next)
-			env = env->next;
+
+	// option 1 on incruste au milieu si possible sinon à la fin
+	env = ft_find_the_insert_spot(env);
+	env_new->next = env->next;
+
+	// // option 2 on incruste à la fin
+	// while (env->next)
+	// 	env = env->next;
+	
 	env->next = env_new;
 	return (start);
 }
